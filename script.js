7855aa73cbc577d4d3229941dd4d8c6deefad17e
Samantha Symes <samantha.symes@outlook.com>
2023-05-30 15:02:08 -0230

    reveal/hide content experament (fail)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// two letter country codes (api)
-const choice = document.querySelector (select);
-console.log(choice);
+// logs two letter country codes
+// const choice = document.getElementsByClassName ('dropdownCon')
+// console.log(choice);
 // const API = 'https://www.travel-advisory.info/api'
 // let feedback;
 // fetch(API)
@@ -10,13 +10,13 @@ console.log(choice);
 //             const code = result.data[list].iso_alpha2;
 
 //             const countryList = document.createElement('p');
-        
-//             countryList.innerHTML = 
+
+//             countryList.innerHTML =
 //                 `${code}`;
 
 //     if (choice === API) {
 //         feedback = "Sucess";
-//     } else { 
+//     } else {
 //         feedback = "fail"
 //     }
 //    const answer = document.createElement ('p')
@@ -27,41 +27,55 @@ console.log(choice);
 //         }
 //     )
 
+// experimenting with adding event listener >> element.addEventListener("click", function);
+const revealBtn = document.querySelector(".reveal-btn");
+const hiddenContent = document.querySelector(".hidden-content");
 
+function revealContent() {
+  if (hiddenContent.classList.contains("reveal-btn")) {
+    hiddenContent.classList.remove("reveal-btn");
+  } else {
+    hiddenContent.classList.add("reveal-btn");
+  }
+}
+// why is revealBtn.addEventListener not working?
+revealBtn.addEventListener("click", revealContent);
+// or
+document.addEventListener("click", revealContent);
 
-// displays all information from API onclick 
-function show() {
-const API_URL = 'https://www.travel-advisory.info/api';
+// displays all information from API onclick
+// function show() {
+// const API_URL = 'https://www.travel-advisory.info/api';
 
-    fetch(API_URL)
-    .then(response => response.json())
-    .then(result => {
-        for (const list in result.data) {
-            
-            const countryName = result.data[list].name;
-            const code = result.data[list].iso_alpha2;
-            const countryScore = result.data[list].advisory.score;
-            const countryMessage = result.data[list].advisory.message;
-            const lastUpdated = result.data[list].advisory.updated;
-            const source = result.data[list].advisory.sources_active;
+//     fetch(API_URL)
+//     .then(response => response.json())
+//     .then(result => {
+//         for (const list in result.data) {
 
-            const countryList = document.createElement('p');
-        
-            countryList.innerHTML = 
-                `<li>${countryName}:
-                    ${code} <br>
-                    ${countryMessage}<br>
-                    ${countryScore}<br>
-                    ${"Updated" + " " + lastUpdated}<br>
-                    ${"From "+ source+" sources"} </li><br>`;
-            document.getElementById('country-list').append(countryList);
-        }
-    });
-    }
+//             const countryName = result.data[list].name;
+//             const code = result.data[list].iso_alpha2;
+//             const countryScore = result.data[list].advisory.score;
+//             const countryMessage = result.data[list].advisory.message;
+//             const lastUpdated = result.data[list].advisory.updated;
+//             const source = result.data[list].advisory.sources_active;
 
+//             const countryList = document.createElement('p');
 
-// create function to display choosen country
-//display country based on dropdown
-//create a function that will take the country name and return the score and message
-//create a function that will take the score and message and return a color
-//associate counrty flag with country name
+//             countryList.innerHTML =
+//                 `<li>${countryName}:
+//                     ${code} <br>
+//                     ${countryMessage}<br>
+//                     ${countryScore}<br>
+//                     ${"Updated" + " " + lastUpdated}<br>
+//                     ${"From "+ source+" sources"} </li><br>`;
+//             document.getElementById('country-list').append(countryList);
+//         }
+//     });
+//     }
+
+// // create function to display choosen country
+
+// //display country based on dropdown
+// //create a function that will take the country name and return the score and message
+// //create a function that will take the score and message and return a color
+// //associate counrty flag with country name
