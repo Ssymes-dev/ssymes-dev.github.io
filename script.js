92e7ad0e8a57e28b48bf68972566507eb144db20
Samantha Symes <samantha.symes@outlook.com>
2023-05-31 09:45:27 -0230

    Event listener working

--- script.js
+++ script.js
@@ -38,40 +38,35 @@ function revealContent() {
     hiddenContent.classList.add("reveal-btn");
   }
 }
-// why is revealBtn.addEventListener not working?
 revealBtn.addEventListener("click", revealContent);
-// or
-document.addEventListener("click", revealContent);
 
 // displays all information from API onclick
-// function show() {
-// const API_URL = 'https://www.travel-advisory.info/api';
+function show() {
+  const API_URL = "https://www.travel-advisory.info/api";
 
-//     fetch(API_URL)
-//     .then(response => response.json())
-//     .then(result => {
-//         for (const list in result.data) {
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((result) => {
+      for (const list in result.data) {
+        const countryName = result.data[list].name;
+        const code = result.data[list].iso_alpha2;
+        const countryScore = result.data[list].advisory.score;
+        const countryMessage = result.data[list].advisory.message;
+        const lastUpdated = result.data[list].advisory.updated;
+        const source = result.data[list].advisory.sources_active;
 
-//             const countryName = result.data[list].name;
-//             const code = result.data[list].iso_alpha2;
-//             const countryScore = result.data[list].advisory.score;
-//             const countryMessage = result.data[list].advisory.message;
-//             const lastUpdated = result.data[list].advisory.updated;
-//             const source = result.data[list].advisory.sources_active;
+        const countryList = document.createElement("p");
 
-//             const countryList = document.createElement('p');
-
-//             countryList.innerHTML =
-//                 `<li>${countryName}:
-//                     ${code} <br>
-//                     ${countryMessage}<br>
-//                     ${countryScore}<br>
-//                     ${"Updated" + " " + lastUpdated}<br>
-//                     ${"From "+ source+" sources"} </li><br>`;
-//             document.getElementById('country-list').append(countryList);
-//         }
-//     });
-//     }
+        countryList.innerHTML = `<li>${countryName}:
+                    ${code} <br>
+                    ${countryMessage}<br>
+                    ${countryScore}<br>
+                    ${"Updated" + " " + lastUpdated}<br>
+                    ${"From " + source + " sources"} </li><br>`;
+        document.getElementById("country-list").append(countryList);
+      }
+    });
+}
 
 // // create function to display choosen country
 
