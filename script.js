80f64adf4d58e10f1a1c588af5c16811d0b6b69b
Samantha Symes <samantha.symes@outlook.com>
2023-06-19 14:35:54 -0230

    started work on second issue

--- script.js
+++ script.js
@@ -3,34 +3,29 @@ const dropdownMenu = document.getElementById("allCountries");
 
 const submitButton = document.querySelector(".btn");
 
+let countryCode = {};
 submitButton.onclick = (e) => {
   e.preventDefault();
-  // display(dropdownMenu.value); how to pass variable now that function is gone
-  console.log(dropdownMenu.value);
+  choice = dropdownMenu.value;
+  console.log(countryCode);
 };
 
-const myList = document.querySelector("ul");
-const apiData = `https://www.travel-advisory.info/api`;
-fetch(apiData)
+let info = {};
+fetch(`https://www.travel-advisory.info/api`)
   .then((response) => response.json())
   .then((result) => {
-    console.log(result.data);
-    for (const country in result.data) {
-      const listItem = document.createElement("li");
-      listItem.appendChild(document.createElement("strong")).textContent =
-        result.data[country].name;
-      listItem.append` ${result.data[country].advisory.message}`;
-      listItem.appendChild(
-        document.createElement("strong")
-      ).textContent = `${result.data[country].advisory.sources_active} sources avaliable`;
-      myList.appendChild(listItem);
-    }
-  })
+    info = result.data;
+    display(countryCode);
+  });
 
-  .catch(console.error);
+// create function to take in countryCode and manupulate info
+// to isolate one object in console
+function display(countryCode) {
+  console.log(info);
+  return info.object === `${countryCode}`;
+}
 
-// // *WIP* parse locally stored API
-
-// //create a function that will take the country name and return the score and message
-// //create a function that will take the score and message and return a color
-// //associate counrty flag with country name
+// *phase 2*
+//create a function that will take the country name and return the score and message
+//create a function that will take the score and message and return a color
+//associate counrty flag with country name
