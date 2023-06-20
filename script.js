0adcbf5387c84030d6244f013b1f2dd68f04f915
Samantha Symes <samantha.symes@outlook.com>
2023-06-20 10:38:10 -0230

    save point-all country messages showing in console

--- script.js
+++ script.js
@@ -14,12 +14,22 @@ let { info } = fetch(`https://www.travel-advisory.info/api`)
   .then((result) => {
     info = result.data;
     display();
+    console.log(Object.keys(info));
   });
 
 // create function to take in countryCode and manupulate info
 // to isolate one object in console
-function display() {
-  console.log(info);
+function display(countryCode) {
+  for (const list in info) {
+    const countryMessage = info[list].advisory.message;
+    let result;
+    if ((countryCode = Object.keys(info))) {
+      result = "sucess";
+    } else {
+      result = "error";
+    }
+    console.log(countryMessage);
+  }
 }
 
 // *phase 2*
