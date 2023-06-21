d31f68a3a21a5e869b185664fedeb7dbe8f446fa
Samantha Symes <samantha.symes@outlook.com>
2023-06-21 11:41:30 -0230

    clears country-list on each new click

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
 //populate dropdown and log choice
-const dropdownMenu = document.getElementById("allCountries");
 
-let countryCode;
+const dropdownMenu = document.getElementById("allCountries");
 const submitButton = document.querySelector(".btn");
+
 submitButton.onclick = (e) => {
   e.preventDefault();
-  getObjectFromAPI(dropdownMenu.value);
-  countryCode = dropdownMenu.value;
-  console.log(dropdownMenu.value);
+  const countryCode = dropdownMenu.value;
+  getObjectFromAPI(countryCode);
+  dropdownMenu.selectedIndex = -1;
+  console.log(countryCode);
 };
 
 // create function to take in countryCode and manupulate info
-let apiData;
 function getObjectFromAPI(countryCode) {
   const apiUrl = `https://www.travel-advisory.info/api?countrycode=${countryCode}`;
   fetch(apiUrl)
     .then((response) => response.json())
-    .then((data) => {
-      apiData = data.data;
-      console.log(apiData);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
+    .then((result) => {
+      const data = result.data;
+      console.log(data);
+      for (const list in data) {
+        const countryName = data[list].name;
+        const code = data[list].iso_alpha2;
+        const countryScore = data[list].advisory.score;
+        const countryMessage = data[list].advisory.message;
+        const lastUpdated = data[list].advisory.updated;
+        const source = data[list].advisory.sources_active;
+
+        const countryList = document.createElement("p");
+
+        document.getElementById("country-list").innerHTML = "";
+
+        countryList.innerHTML = `<li>${countryName}:
+                    ${code} <br>
+                    ${countryMessage}<br>
+                    ${countryScore}<br>
+                    ${"Updated" + " " + lastUpdated}<br>
+                    ${"From " + source + " sources"} </li><br>`;
+        document.getElementById("country-list").appendChild(countryList);
+      }
     });
 }
-getObjectFromAPI("countryCode");
 
 // *phase 2*
 //create a function that will take the country name and return the score and message
