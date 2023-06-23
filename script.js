10bb95e42ff166f2c6679167aa5dc1bafa2de6ab
Ssymes-dev <119711298+Ssymes-dev@users.noreply.github.com>
2023-06-23 11:18:36 -0230

    Test branch (#9)
    * test
    
    * test
    
    * changed background color to seafoam

--- script.js
+++ script.js
@@ -6,39 +6,45 @@ const submitButton = document.querySelector(".btn");
 submitButton.onclick = (e) => {
   e.preventDefault();
   const countryCode = dropdownMenu.value;
-  getObjectFromAPI(countryCode);
+  apiObject(countryCode);
   dropdownMenu.selectedIndex = -1;
   console.log(countryCode);
 };
 
-// create function to take in countryCode and manupulate info
-function getObjectFromAPI(countryCode) {
+// create function to take in countryCode and manupulate api object
+function apiObject(countryCode) {
   const apiUrl = `https://www.travel-advisory.info/api?countrycode=${countryCode}`;
   fetch(apiUrl)
     .then((response) => response.json())
     .then((result) => {
       const data = result.data;
       console.log(data);
-      for (const list in data) {
-        const countryName = data[list].name;
-        const code = data[list].iso_alpha2;
-        const countryScore = data[list].advisory.score;
-        const countryMessage = data[list].advisory.message;
-        const lastUpdated = data[list].advisory.updated;
-        const source = data[list].advisory.sources_active;
+      const countryList = document.getElementById("country-list");
 
-        const countryList = document.createElement("p");
+      // Clear previous content
+      countryList.innerHTML = "";
 
-        document.getElementById("country-list").innerHTML = "";
+      for (const country in data) {
+        const listItem = document.createElement("p");
 
-        countryList.innerHTML = `<li>${countryName}:
-                    ${code} <br>
-                    ${countryMessage}<br>
-                    ${countryScore}<br>
-                    ${"Updated" + " " + lastUpdated}<br>
-                    ${"From " + source + " sources"} </li><br>`;
-        document.getElementById("country-list").appendChild(countryList);
+        // Create countryNae title
+        const countryName = document.createElement("h1");
+        countryName.textContent = data[country].name;
+        listItem.appendChild(countryName);
+
+        // Append advisory message
+        listItem.append(`${data[country].advisory.message}`);
+
+        // Create <strong> element for sources
+        const sources = document.createElement("strong");
+        sources.textContent = ` ${data[country].advisory.sources_active} sources available.`;
+        listItem.appendChild(sources);
+
+        countryList.appendChild(listItem);
       }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
     });
 }
 
