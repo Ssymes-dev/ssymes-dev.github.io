1fcb9502f1636caeca051a524e505baeda8c9999
Samantha Symes <samantha.symes@outlook.com>
2023-06-26 12:20:44 -0230

    dropdown menu populated with API

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const dropdownMenu = document.getElementById("allCountries");
 const submitButton = document.querySelector(".btn");
 
+getCountries();
 submitButton.onclick = (e) => {
   e.preventDefault();
   const countryCode = dropdownMenu.value;
@@ -10,6 +11,27 @@ submitButton.onclick = (e) => {
   dropdownMenu.selectedIndex = -1;
   console.log(countryCode);
 };
+
+// function to populate dropdown menu
+function getCountries() {
+  const apiUrl = "https://www.travel-advisory.info/api";
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((result) => {
+      const data = result.data;
+
+      for (const list in data) {
+        const countryName = data[list].name;
+        const option = document.createElement("option");
+        option.value = list;
+        option.textContent = countryName;
+        dropdownMenu.appendChild(option);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
 
 // create function to take in countryCode and manupulate info
 function getObjectFromAPI(countryCode) {
