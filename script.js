f22c730d50d9b9036482e2aa25a0c04703542b4e
Samantha Symes <samantha.symes@outlook.com>
2023-07-09 20:20:34 -0230

    weather works?

--- script.js
+++ script.js
@@ -79,7 +79,6 @@ function apiObject(countryCode) {
 }
 
 // map
-// map
 var map = L.map("map").setView([51.505, -0.09], 13);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -87,6 +86,9 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+L.tileLayer(
+  `https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=efa153cb7f3aabbfc22da92129ec3413`
+).addTo(map);
 // *phase 2*
 //create a function that will take the country name and return the score and message
 //create a function that will take the score and message and return a color
