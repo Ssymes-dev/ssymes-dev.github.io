06ded6a63ebb673d685f123c28d3e98507bb54a6
Samantha Symes <samantha.symes@outlook.com>
2023-08-09 17:27:38 -0230

    previous save caused bug. fixed

--- script.js
+++ script.js
@@ -197,7 +197,7 @@ function generatePopupContent(travelCountryCode) {
 
     const popupContent = `
       <div>
-        <h3>${selectedCountryData.name}</h3>
+        <h2>${selectedCountryData.name}</h2>
         <p>Advisory: ${advisory.message}</p>
         <a href="${
           advisory.source
