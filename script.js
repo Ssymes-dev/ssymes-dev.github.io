4e90818d45904b26c393dfe9640ece3f1c69e8a2
Samantha Symes <samantha.symes@outlook.com>
2023-08-17 19:18:48 -0230

    added console logs and comments

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Global variables
 const countryDropdown = document.getElementById("allCountries");
 const countrySearchInput = document.getElementById("countrySearchInput");
-let allCountryOptions = [];
-let countryDataCache = null;
-let windyAPI;
+let allCountryOptions = []; // To store the list of all country options
+let countryDataCache = null; // To cache fetched country data
+let windyAPI; // Windy API object for map interaction
 
 // Initialization
 const initWindyOptions = {
@@ -14,9 +14,15 @@ const initWindyOptions = {
   zoom: 0,
 };
 
-windyInit(initWindyOptions);
+// Initialize Windy API with provided options
+console.log("Initializing Windy API...");
+windyInit(initWindyOptions, (api) => {
+  windyAPI = api; // Store the initialized API object in the global variable
+  console.log("Windy API initialized:", windyAPI);
+});
 
 $(document).ready(function () {
+  // Show the 'About' modal when the link is clicked
   $("#aboutLink").click(function () {
     $("#aboutProjectModal").modal("show");
   });
@@ -28,18 +34,22 @@ countryDropdown.parentElement.addEventListener("click", async (event) => {
     const selectedTravelCountryCode =
       event.target.getAttribute("data-country-code");
     if (selectedTravelCountryCode) {
+      // Set the search input value and selected code attribute
       countrySearchInput.value = event.target.textContent;
-      console.log("countrySearchInput.value", countrySearchInput.value);
+      console.log("Set search input value:", countrySearchInput.value);
       countrySearchInput.setAttribute(
         "data-selected-code",
         selectedTravelCountryCode
       );
-      countrySearchInput.value = "";
+      countrySearchInput.value = ""; // Reset search input value
 
+      // Find the selected option based on the country code
       const selectedOption = allCountryOptions.find(
         ({ code }) => code === selectedTravelCountryCode
       );
+
       if (selectedOption) {
+        // Update dropdown value, fetch and display country details, update map
         countryDropdown.value = selectedOption.code;
         const travelData = await getCachedCountryData(selectedOption.code);
         displayCountryDetails(travelData);
@@ -59,31 +69,21 @@ countrySearchInput.addEventListener("input", async () => {
       option.code.toLowerCase().includes(searchTerm)
   );
   appendDropdownOptions(filteredOptions);
+  console.log("Filtered options based on search:", filteredOptions);
 });
 
 // Event listener for the search input click
 countrySearchInput.addEventListener("click", async () => {
-  countrySearchInput.value = "";
-  allCountryOptions = [];
+  filteredOptions = [];
+  console.log("Cleared filteredOptions array");
+
   // If the search input is empty, populate the country dropdown
   if (countrySearchInput.value.trim() === "") {
-    console.log(
-      "countrySearchInput.value.trim",
-      countrySearchInput.value.trim()
-    );
+    console.log("Populating country dropdown...");
     await populateCountryDropdown();
   }
+  console.log("Populated country dropdown");
 });
-
-// function googleTranslateElementInit() {
-//   new google.translate.TranslateElement(
-//     {
-//       pageLanguage: "en",
-//       layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-//     },
-//     "google_translate_element"
-//   );
-// }
 
 // Function to fetch country data
 async function fetchCountryData() {
@@ -104,6 +104,10 @@ async function populateCountryDropdown() {
     const travelData = await fetchCountryData();
     allCountryOptions = alphabetizeCountries(travelData);
     appendDropdownOptions(allCountryOptions);
+    console.log(
+      "Populated allCountryOptions with travel data:",
+      allCountryOptions
+    );
   } catch (error) {
     console.error("Error fetching country data:", error);
   }
@@ -135,6 +139,7 @@ async function getCachedCountryData(travelCountryCode) {
   try {
     const travelData = await fetchCountryData();
     countryDataCache = travelData;
+    console.log("Cached country data in countryDataCache:", countryDataCache);
     return countryDataCache[travelCountryCode];
   } catch (error) {
     console.error("Error fetching country data:", error);
@@ -144,14 +149,18 @@ async function getCachedCountryData(travelCountryCode) {
 
 // Function to set map location and add a marker with popup
 function setMapLocation(lat, lon, popupContent, bounds) {
+  // Check if the Windy API is initialized
   if (!windyAPI) {
     console.error("Windy API not initialized!");
     return;
   }
 
+  // Get the Windy map instance
   const map = windyAPI.map;
 
+  // Check if bounds information is provided
   if (bounds && bounds.northeast && bounds.southwest) {
+    // Extract coordinates from bounds information
     const southWestLatLng = L.latLng(
       bounds.southwest.lat,
       bounds.southwest.lng
@@ -160,11 +169,17 @@ function setMapLocation(lat, lon, popupContent, bounds) {
       bounds.northeast.lat,
       bounds.northeast.lng
     );
+
+    // Create a bounding box using the extracted coordinates
     const boundingBox = L.latLngBounds(southWestLatLng, northEastLatLng);
 
+    // Fit the map to the bounding box
     map.fitBounds(boundingBox);
 
+    // Call the addMarker function to add a marker with popup
     addMarker(map, lat, lon, popupContent);
+  } else {
+    console.error("Bounds information not provided!");
   }
 }
 
@@ -175,19 +190,20 @@ function addMarker(map, lat, lon, popupContent) {
     return;
   }
 
-  // Check if a marker already exists on the map
+  // Remove existing markers from the map
   map.eachLayer((layer) => {
     if (layer instanceof L.Marker) {
       layer.remove();
     }
   });
 
+  // Add a new marker to the map
   const marker = L.marker([lat, lon]).addTo(map);
-  const popup = L.popup({
-    // maxWidth: 275,
-    // maxHeight: 275,
-  }).setContent(popupContent);
 
+  // Create a popup for the marker
+  const popup = L.popup().setContent(popupContent);
+
+  // Bind the popup to the marker and open it
   marker.bindPopup(popup).openPopup();
 }
 
@@ -195,9 +211,12 @@ function addMarker(map, lat, lon, popupContent) {
 function addEventListenersToOptions() {
   const dropdownOptions = document.querySelectorAll(".dropdown-item");
   dropdownOptions.forEach((option) => {
+    // Add a click event listener to each dropdown option
     option.addEventListener("click", async () => {
       const selectedTravelCountryCode =
         option.getAttribute("data-country-code");
+
+      // Check if a country code is available
       if (selectedTravelCountryCode) {
         countrySearchInput.value = option.textContent;
         countrySearchInput.setAttribute(
@@ -205,13 +224,19 @@ function addEventListenersToOptions() {
           selectedTravelCountryCode
         );
 
+        // Find the selected country option
         const selectedOption = allCountryOptions.find(
           ({ code }) => code === selectedTravelCountryCode
         );
+
+        // Display country details and update the map
         if (selectedOption) {
           countryDropdown.value = selectedOption.code;
           const travelData = await getCachedCountryData(selectedOption.code);
           displayCountryDetails(travelData);
+
+          // Log the selected country's details
+          console.log("Selected Country Details:", travelData);
         }
       }
     });
@@ -220,18 +245,22 @@ function addEventListenersToOptions() {
 
 // Helper function to generate popup content
 function generatePopupContent(travelCountryCode) {
+  // Check if country data cache is available
   if (!countryDataCache) {
     return "";
   }
 
+  // Retrieve selected country's data from cache
   const selectedCountryData = countryDataCache[travelCountryCode];
   if (selectedCountryData) {
     const advisory = selectedCountryData.advisory;
 
+    // Handle sources_active as an array or single source
     const sourcesActive = Array.isArray(advisory.sources_active)
       ? advisory.sources_active
       : [advisory.sources_active];
 
+    // Construct popup content
     const popupContent = `
         <h5>${selectedCountryData.name}</h5>
         <p class="advisory-message">Advisory: ${advisory.message}</p>
@@ -241,9 +270,9 @@ function generatePopupContent(travelCountryCode) {
         }" target="_blank" rel="noopener noreferrer">Sources: ${createSourcesText(
       sourcesActive
     )}</a>
-
     `;
 
+    // Log the generated popup content
     console.log("Popup Content:", popupContent);
 
     return popupContent;
@@ -254,6 +283,7 @@ function generatePopupContent(travelCountryCode) {
 
 // Function to create sources text
 function createSourcesText(sources) {
+  // Combine sources with commas and return
   return sources.join(", ");
 }
 
@@ -266,6 +296,7 @@ async function updateMapWithGeocoding(travelCountryCode) {
     );
     const geocodingApiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${selectedTravelOption.name}&key=${OPEN_CAGE_API_KEY}`;
 
+    // Fetch geocoding data from the API
     const response = await fetch(geocodingApiUrl);
     const { results } = await response.json();
 
@@ -281,7 +312,7 @@ async function updateMapWithGeocoding(travelCountryCode) {
   }
 }
 
-// Function to display country details
+// Function to display country details on the page
 async function displayCountryDetails(travelData) {
   const countryListElement = document.getElementById("country-list");
   const travelCountryCode = countryDropdown.value;
@@ -295,6 +326,7 @@ async function displayCountryDetails(travelData) {
 
     if (travelCountryCode !== "") {
       try {
+        // Update the map with geocoding data
         await updateMapWithGeocoding(travelCountryCode);
       } catch (error) {
         console.error("Error updating map with geocoding:", error);
@@ -302,25 +334,32 @@ async function displayCountryDetails(travelData) {
     }
   }
 }
+
 // Helper function to alphabetize country options
 function alphabetizeCountries(travelData) {
+  // Create an array of country options with code and name properties
   const countryOptions = Object.keys(travelData).map((travelCountryCode) => ({
     code: travelCountryCode,
     name: travelData[travelCountryCode].name,
   }));
+
+  // Sort country options alphabetically by name
   return countryOptions.sort((a, b) => a.name.localeCompare(b.name));
 }
 
+// Function to initialize the Windy API
 function initWindy() {
   windyInit(initWindyOptions, (api) => {
     windyAPI = api; // Store the initialized API object in the global variable
+    console.log("Windy API initialized:", windyAPI);
   });
 }
 
 // Initial population of the country dropdown and Windy API initialization
+console.log("Initializing Windy API and populating country dropdown...");
 populateCountryDropdown();
-
 initWindy();
 
 // Add event listener to the country dropdown options
+console.log("Adding event listeners to country dropdown options...");
 addEventListenersToOptions();
