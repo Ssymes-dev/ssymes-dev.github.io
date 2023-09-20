89fc0474ec8c897484ade80e9ac1c5067972a75a
Samantha Symes <samantha.symes@outlook.com>
2023-09-20 11:52:53 -0230

    modal for no match finished

--- script.js
+++ script.js
@@ -67,7 +67,6 @@ async function setDropdownOptions(locationName, dropdownOptions) {
   }
 }
 
-// get location data
 async function getLocationData(lng, lat) {
   const OPEN_CAGE_API_KEY = "0c9aade54fba4c8abfae724859a72795";
   const reverseGeocodingApiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${lat},${lng}&key=${OPEN_CAGE_API_KEY}`;
@@ -75,7 +74,13 @@ async function getLocationData(lng, lat) {
   const response = await fetch(reverseGeocodingApiUrl);
   const { results } = await response.json();
   console.log("results", results);
-  const locationName = results[0].components.country;
+  let locationName = results[0].components.country;
+
+  // Handle cases where locationName is undefined
+  if (locationName === undefined) {
+    locationName = results[0].formatted;
+  }
+
   console.log("location Name", locationName);
   return locationName;
 }
@@ -120,7 +125,7 @@ async function generateAdvisoryContent(countryData, selectedCountry) {
         previousModal.remove();
       }
 
-      const modalContent = `
+      let modalContent = `
         <div class="modal fade" id="advisoryModal" tabindex="-1" role="dialog" aria-labelledby="advisoryModalLabel" aria-hidden="true">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -133,6 +138,27 @@ async function generateAdvisoryContent(countryData, selectedCountry) {
               <div class="modal-body">
                 <p class="advisory-message">Advisory: ${advisory.message}</p>
                 <a href="${advisory.source}" target="_blank" rel="noopener noreferrer">Source: ${advisory.sources_active}</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+
+      document.body.insertAdjacentHTML("beforeend", modalContent);
+      $("#advisoryModal").modal("show");
+    } else if (name !== selectedCountry) {
+      modalContent = `
+        <div class="modal fade" id="advisoryModal" tabindex="-1" role="dialog" aria-labelledby="advisoryModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="advisoryModalLabel">${selectedCountry}</h5>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p class="advisory-message">There are currently no advisories available for ${selectedCountry}</p>
               </div>
             </div>
           </div>
